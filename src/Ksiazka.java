9e29bf6d38262463dc5e070bd15053b9a04482d6
s36713 <s36713@pjwstk.edu.pl>
2026-03-22 22:39:54 +0100

    Dodanie zadania 1

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.liczbaStron = liczbaStron;
+        this.dostepna = dostepna;
+
+
+    }
+
+    public void wypiszInfo() {
+        System.out.println("Tytul:" + this.tytul);
+        System.out.println("Autor:" + this.autor);
+        System.out.println("Liczba stron:" + this.liczbaStron);
+        System.out.println("Dostepna:" + this.dostepna);
+
+    }
+
+    boolean wypozycz() {
+        if (dostepna) {
+            System.out.println("Ksiazka jest dostepna.");
+            return dostepna = false;
+
+        } else {
+            System.out.println("Ksiazka jest niedostepna");
+            return false;
+
+        }
+
+
+    }
+
+    void zwroc() {
+        System.out.println("Dziekujem za zwrot!");
+    }
 }
